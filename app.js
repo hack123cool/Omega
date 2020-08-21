f0c68f6fcc213e8c7f2e87ac1fef0b935286b96f
hack123cool <70032502+hack123cool@users.noreply.github.com>
2020-08-21 23:54:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAYjyhNYy4nggyU3yNotU30dBomCjH_i7E",
+  authDomain: "hacking-f7c1b.firebaseapp.com",
+  databaseURL: "https://hacking-f7c1b.firebaseio.com",
+  projectId: "hacking-f7c1b",
+  storageBucket: "hacking-f7c1b.appspot.com",
+  messagingSenderId: "234003080984",
+  appId: "1:234003080984:web:0915f811cf33e78bdea06d",
+  measurementId: "G-6JLWFM6LQ7"
 };
 
      
